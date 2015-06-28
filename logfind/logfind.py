bfc2bc5911f211dc8aecb1f86f189928d267b26f
Deon Heyns <heynsd@gmail.com>
2015-06-28 11:17:14 -0300

    Add logic to read .logfile, read log files and find text

---
+++ logfind/logfind.py
@@ -0,0 +1,90 @@
+__author__ = 'deonheyns'
+import os
+import glob
+import re
+
+class Logfind(object):
+    def __init__(self):
+        self.__dot_logfind = '.logfind'
+
+    # method to read .logfind to obtain the log files that are important
+    # presume .logfind will be in ~ also try logfind folder then executing folder
+    def read_dot_logfind(self):
+        important_files = []
+        errors = []
+        try:
+            important_files.append(self.__read_dot_logfind_from_home_directory())
+        except IOError as ex:
+            errors.append(ex)
+
+        try:
+            important_files.append(self.__read_dot_logfind_from_logfind_directory())
+        except IOError as ex:
+            errors.append(ex)
+
+        try:
+            important_files.append(self.__read_dot_logfind_from_executing_directory())
+        except IOError as ex:
+            errors.append(ex)
+
+        if len(important_files) == 0 and len(errors) > 0:
+            raise Exception(errors)
+
+        important_files = self.__flatten(important_files)
+        return important_files
+
+    # method to scan current directory for all files that match the .logfind criteria
+    def get_log_files(self, logs_patterns):
+        results = []
+        executing_directory = os.getcwd()
+        for x in logs_patterns:
+            results.append(glob.glob(os.path.join(executing_directory, x)))
+
+        results = self.__flatten(results)
+        return results
+
+    # read the passed in logfiles and find the passed in value
+    def read_log_files(self, log_files, text, treat_as_or=False):
+        results = []
+        regex = self.__create_regex_pattern(text, treat_as_or)
+        for log_file in log_files:
+            with file(log_file, "r") as f:
+                contents = f.read()
+                search = re.search(regex, contents, re.M | re.I)
+                if search:
+                    results.append(log_file)
+        return results
+
+    def __read_dot_logfind_from_home_directory(self):
+        home_directory = os.environ['HOME']
+        directory = os.path.join(home_directory, self.__dot_logfind)
+        with file(directory, "r") as f:
+            return f.readlines()
+
+    def __read_dot_logfind_from_logfind_directory(self):
+        logfind_directory = os.path.dirname(os.path.realpath(__file__))
+        directory = os.path.join(logfind_directory, self.__dot_logfind)
+        with file(directory) as f:
+            return f.readlines()
+
+    def __read_dot_logfind_from_executing_directory(self):
+        executing_directory = os.getcwd()
+        directory = os.path.join(executing_directory, self.__dot_logfind)
+        with file(directory) as f:
+            return f.readlines()
+
+    def __create_regex_pattern(self, text, treat_as_or=False):
+        if not treat_as_or:
+            regex = r'(?=.*?\b{0}\b.*?)'
+            pattern = ''.join([regex.format(x) for x in text.split()])
+            return pattern
+
+        else:
+            regex = r'(?=.*?\b{0}\b.*?)'
+            pattern = '|'.join([regex.format(x) for x in text.split()])
+            return pattern
+
+    @staticmethod
+    def __flatten(items):
+        flatten = [item for sublist in items for item in sublist]
+        return flatten
--- tests/logfind_tests.py
+++ tests/logfind_tests.py
@@ -8,6 +8,85 @@ def setup():
 def teardown():
     print "TEAR DOWN!"
 
-def test_basic():
-    print "I RAN!"
-    
+def test_read_important_files_from_dotlogfind():
+    # arrange
+    lf = logfind.Logfind()
+
+    # act
+    patterns = lf.read_dot_logfind()
+
+    # assert
+    assert_equal(1, len(patterns))
+
+def test_return_files_based_on_important_files_from_dotlogfind():
+    # arrange
+    lf = logfind.Logfind()
+    patterns = lf.read_dot_logfind()
+
+    # act
+    log_files = lf.get_log_files(patterns)
+
+    # assert
+    assert_equal(1, len(patterns))
+    assert_equal(1, len(log_files))
+
+def test_can_return_logfiles_that_contain_text():
+    # arrange
+    lf = logfind.Logfind()
+    patterns = lf.read_dot_logfind()
+    log_files = lf.get_log_files(patterns)
+    text = 'important'
+
+    # act
+    matches = lf.read_log_files(log_files, text)
+
+    # assert
+    assert_equal(1, len(patterns))
+    assert_equal(1, len(log_files))
+    assert_equal(1, len(matches))
+
+def test_returns_logfiles_that_contain_all_text():
+    # arrange
+    lf = logfind.Logfind()
+    patterns = lf.read_dot_logfind()
+    log_files = lf.get_log_files(patterns)
+    text = 'some important text'
+
+    # act
+    matches = lf.read_log_files(log_files, text)
+
+    # assert
+    assert_equal(1, len(patterns))
+    assert_equal(1, len(log_files))
+    assert_equal(1, len(matches))
+
+def test_does_not_return_logfiles_that_do_not_contain_all_text():
+    # arrange
+    lf = logfind.Logfind()
+    patterns = lf.read_dot_logfind()
+    log_files = lf.get_log_files(patterns)
+    text = 'important files'
+
+    # act
+    matches = lf.read_log_files(log_files, text)
+
+    # assert
+    assert_equal(1, len(patterns))
+    assert_equal(1, len(log_files))
+    assert_equal(0, len(matches))
+
+def test_returns_logfiles_when_treating_text_separation_as_or():
+    # arrange
+    lf = logfind.Logfind()
+    patterns = lf.read_dot_logfind()
+    log_files = lf.get_log_files(patterns)
+    text = 'important files'
+
+    # act
+    matches = lf.read_log_files(log_files, text, treat_as_or=True)
+
+    # assert
+    assert_equal(1, len(patterns))
+    assert_equal(1, len(log_files))
+    assert_equal(1, len(matches))
+
